f485642ae270ad1bf75debc0733a10904fdb7360
Timothy Weinhardt <timothy@Timothys-MBP.lan>
2024-02-23 15:24:50 -0500

    Add dynamic background tile generation
    Create generate_background_tiles() function to add BackgroundTile sprites around player

--- scripts/main.py
+++ scripts/main.py
@@ -25,17 +25,26 @@ player = Player((config.SCREEN_WIDTH/2, config.SCREEN_HEIGHT/2), camera_group)
 #
 # Tile generation
 #
-
-# Background tiles (ex. Grass)
-for i in range(config.TILE_ROWS):
-    for j in range(config.TILE_COLS):
-        BackgroundTile(choice(['grass-01', 'grass-02']), (j * config.TILE_SIZE, i * config.TILE_SIZE), camera_group)
+existing_tiles = set()
 
 # Foreground tiles (ex. Rocks)
-for i in range(3):
+for i in range(10):
     y_pos = randint(0, config.TILE_ROWS)
     x_pos = randint(0, config.TILE_COLS)
     ForegroundTile('rock-01', (x_pos * config.TILE_SIZE, y_pos * config.TILE_SIZE), camera_group)
+
+# Create tiles around player
+def generate_background_tiles(x, y, distance_x, distance_y):
+    player_grid_x = x // config.TILE_SIZE
+    player_grid_y = y // config.TILE_SIZE
+    for x in range(-distance_x, distance_x + 1):
+        for y in range(-distance_y, distance_y + 1):
+            tile_grid_x = player_grid_x + x
+            tile_grid_y = player_grid_y + y
+            # Check if there is a tile at this position
+            if (tile_grid_x, tile_grid_y) not in existing_tiles:
+                BackgroundTile(choice(['grass-01', 'grass-02']), (tile_grid_x * config.TILE_SIZE, tile_grid_y * config.TILE_SIZE), camera_group)
+                existing_tiles.add((tile_grid_x, tile_grid_y))
 
 #
 # Timers
@@ -51,6 +60,7 @@ while True:
             pygame.quit()
             exit()
 
+    generate_background_tiles(player.position[0], player.position[1], config.TILE_COLS, config.TILE_ROWS)
     screen.fill('#ffffff')
     camera_group.update()
     camera_group.custom_draw(player)
